3834f96854d1845a63d5b4305104be4b9efceece
Guido van Biemen <guidovanbiemen@gmail.com>
2018-06-23 17:06:28 +0200

    HTML Element class.

---
+++ classes/Element.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Neat\View;
+
+class Element
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string[]
+     */
+    protected $attributes;
+
+    /**
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * Element constructor
+     *
+     * @param string   $name
+     * @param string[] $attributes
+     * @param mixed    $content
+     */
+    public function __construct(string $name, array $attributes = [], $content = null)
+    {
+        $this->name       = $name;
+        $this->attributes = $attributes;
+        $this->content    = $content;
+    }
+
+    /**
+     * Render element
+     *
+     * @return mixed
+     */
+    public function __toString()
+    {
+        if ($this->content === null) {
+            return $this->open();
+        }
+
+        return $this->open() . $this->content() . $this->close();
+    }
+
+    /**
+     * Escape content
+     *
+     * @param string $content
+     * @return string
+     */
+    protected function escape($content)
+    {
+        return htmlspecialchars($content, ENT_COMPAT | ENT_HTML5, 'UTF-8');
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get attributes
+     *
+     * @return string
+     */
+    public function attributes()
+    {
+        $attributes = $this->attributes;
+        array_walk($attributes, function (&$value, $key) {
+            if (!is_int($key)) {
+                $value = sprintf(' %s="%s"', $key, $this->escape($value));
+            } else {
+                $value = ' ' . $value;
+            }
+        });
+
+        return implode(' ', $attributes);
+    }
+
+    /**
+     * Get open tag
+     *
+     * @return string
+     */
+    public function open()
+    {
+        return '<' . $this->name() . $this->attributes() . '>';
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function content()
+    {
+        return $this->escape($this->content);
+    }
+
+    /**
+     * Get close tag
+     *
+     * @return string
+     */
+    public function close()
+    {
+        return '</' . $this->name() . '>';
+    }
+}
---
+++ tests/ElementTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Neat\View\Test;
+
+use Neat\View\Element;
+use PHPUnit\Framework\TestCase;
+
+class ElementTest extends TestCase
+{
+    /**
+     * Test empty element
+     */
+    public function testEmpty()
+    {
+        $element = new Element('br');
+
+        $this->assertSame('<br>', (string) $element);
+    }
+
+    /**
+     * Test attributes
+     */
+    public function testAttributes()
+    {
+        $this->assertSame('<input required>', (string) new Element('input', ['required']));
+        $this->assertSame('<input type="text">', (string) new Element('input', ['type' => 'text']));
+        $this->assertSame(
+            '<input value="&lt;&gt;&quot;&amp;\'">',
+            (string) new Element('input', ['value' => '<>"&\''])
+        );
+    }
+
+    /**
+     * Test content
+     */
+    public function testContent()
+    {
+        $this->assertSame('<div>Hello world!</div>', (string) new Element('div', [], 'Hello world!'));
+        $this->assertSame('<div>&lt;&gt;&quot;&amp;\'</div>', (string) new Element('div', [], '<>"&\''));
+    }
+}
